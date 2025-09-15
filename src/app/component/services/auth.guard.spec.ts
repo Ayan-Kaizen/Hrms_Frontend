014667f835e48396a81da9de59a010baed0523a5
Ayan <ayan.yusufmail@gmail.com>
2025-09-15 17:04:37 +0530

    aa

--- src/app/component/services/auth.guard.spec.ts
+++ src/app/component/services/auth.guard.spec.ts
@@ -1,20 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthGuard } from './auth.gaurd';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { authGuard } from './auth.guard';
-
-
-
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [AuthGuard]
+    });
+    guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('should return true for canActivate', () => {
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    expect(guard.canActivate(route, state)).toBe(true); // adjust based on your logic
   });
 });
